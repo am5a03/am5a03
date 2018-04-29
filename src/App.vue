23a6cf8974a7ba8e00bb31b42d5a722284c86620
Raymond Chan <am5a03@gmial.com>
2018-04-30 01:00:26 +0800

    Basic wireframe

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,20 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <nav id="nav">
+      <ul class="nav-default">
+        <li>Home</li>
+        <li>About me</li>
+        <li>Contacts</li>
+      </ul>
+    </nav>
+    <main>
+      <h1>
+        Hi, this is Raymond
+      </h1>
+      <p>
+        I am a fullstack developer, with solid experience in Android development.
+      </p>
+    </main>
   </div>
 </template>
 
@@ -12,12 +25,20 @@ export default {
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+} */
+
+h1 {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  font-size: 10em;
+  margin: 0;
+  padding: 0;
 }
 </style>
