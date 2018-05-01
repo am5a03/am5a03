e3125b055bb652411121a902c8b8ed90a04943d5
Raymond Chan <am5a03@gmial.com>
2018-05-01 17:12:08 +0800

    Updated font family

--- src/App.vue
+++ src/App.vue
@@ -14,15 +14,16 @@
       </ul>
     </nav>
     <main>
-      <div id="about">
-        <h1>
-          Hi, this is Raymond
-        </h1>
-        <p>
-          I am a fullstack developer, with solid experience in Android development.
-        </p>
+      <div id="main" class="page">
+        <h1>Hi, this is Raymond</h1>
       </div>
-      <div id="contacts">
+      <div id="about" class="page">
+        <h2>About me</h2>
+        <p>I am a fullstack developer based in Hong Kong, with solid experience in Android development.</p>
+        <p>I have been working @9GAG for several years, having hands on experiences with various developments and frameworks. Including API design, system architecture design and currently focusing on Android development.</p>
+      </div>
+      <div id="contacts" class="page">
+        <h2>Contacts</h2>
         <ul>
           <li>
             <a href="https://www.facebook.com/dnomyar.tc" target="_blank">Facebook</a>
@@ -68,4 +69,29 @@ export default {
   margin: 0;
   padding: 0;
 } */
+
+body {
+  margin: 0 auto;
+  font-family: 'Questrial', sans-serif;
+}
+
+.page {
+  height: 100vh;
+}
+
+nav {
+  background-color: #333;
+  overflow: hidden;
+}
+
+nav ul {
+  margin: 0;
+}
+
+nav ul > li {
+  float: left;
+  display: block;
+  padding: 16px 16px;
+}
+
 </style>
